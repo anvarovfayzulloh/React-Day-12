e292fb18d92dc91a4b6af49b4c85adee82006645
anvarovfayzulloh <fayzee200708@gmail.com>
2024-08-23 03:06:21 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useLocation } from "react-router-dom"
 import RouterController from "./routes"
-
+import { useEffect } from "react"
 
 function App() {
+  const {pathname} = useLocation()
 
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [pathname])
   return (
     <>
       <RouterController/>
